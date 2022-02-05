6dca192991b58b5f9f9801e72e0bee77eb9eafb4
emanuel <76693837+emsquid@users.noreply.github.com>
2022-02-05 14:26:25 +0100

    Update 22-NSI-15.py

--- Sujet 15/22-NSI-15.py
+++ Sujet 15/22-NSI-15.py
@@ -12,7 +12,7 @@ def nb_repetitions(elt, tab:list)->int:
 def binaire(a):
     bin_a = str(a%2) # on ajoute le premier bit 
     a = a // 2
-    while a != 0:
+    while a > 0:
         bin_a = str(a%2) + bin_a # on ajoute les bits suivants devant
         a = a // 2 # on effectue la divison euclidienne de a par 2
     return bin_a
